7b2256c448c69f622f9440174a4f75b91db58737
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2025-12-01 11:11:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1022,7 +1022,7 @@ async def escrow(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     elements.append(Paragraph(
         f"💰 <b>Total Escrow Volume:</b> ₹{total_amount:.2f}<br/><br/>"
-        "💼 Trusify via Lucky Escrow Bot",
+        "💼 Trusify Escrow Bot",
         footer_style
     ))
 
